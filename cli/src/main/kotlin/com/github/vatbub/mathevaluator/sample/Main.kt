a904aaacdbe91a6dd190ecad6ea852f22f34f2ac
Frederik Kammel <frederik.kammel@ipm.fraunhofer.de>
2022-10-09 23:14:42 +0200

    Translated the sample CLI to Kotlin

--- cli/src/main/kotlin/com/github/vatbub/mathevaluator/sample/Main.kt
+++ cli/src/main/kotlin/com/github/vatbub/mathevaluator/sample/Main.kt
@@ -1,5 +1,4 @@
-package com.github.vatbub.mathevaluator.sample;
-
+package com.github.vatbub.mathevaluator.sample
 /*-
  * #%L
  * math-evaluator
@@ -9,9 +8,9 @@ package com.github.vatbub.mathevaluator.sample;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,33 +18,29 @@ package com.github.vatbub.mathevaluator.sample;
  * limitations under the License.
  * #L%
  */
+import com.github.vatbub.commandlineUserPromptProcessor.Prompt
+import com.github.vatbub.commandlineUserPromptProcessor.parsers.StringParser
+import com.github.vatbub.mathevaluator.toMathExpression
 
+const val exitInput = "exit"
 
-import com.github.vatbub.commandlineUserPromptProcessor.Prompt;
-import com.github.vatbub.commandlineUserPromptProcessor.parsables.ParsableString;
-import com.github.vatbub.commandlineUserPromptProcessor.parsables.ParseException;
-import com.github.vatbub.mathevaluator.MathExpression;
-
-public class Main {
-    public static final String exitInput = "exit";
-
-    public static void main(String[] args) throws ParseException {
-        while (true) {
-            Prompt prompt = new Prompt("Please enter your math expression (Hit Ctrl+C or type 'exit' to quit the program)", new ParsableString(exitInput));
-            ParsableString input = (ParsableString) prompt.doPrompt();
-            if (input.toValue().equals(exitInput))
-                break;
-
-            try {
-                String inputValue = input.toValue();
-                MathExpression expression = new MathExpression(inputValue);
-                if (inputValue.contains("="))
-                    System.out.println("Variable successfully defined, variable currently evaluates to " + expression.evaluate().getValue());
-                else
-                    System.out.println("The result is " + expression.evaluate().getValue());
-            } catch (Exception e) {
-                e.printStackTrace();
+fun main() {
+    while (true) {
+        val prompt = Prompt(
+            "Please enter your math expression (Hit Ctrl+C or type 'exit' to quit the program)",
+            StringParser()
+        )
+        val input = prompt.doPrompt(exitInput)
+        if (input == exitInput) break
+        try {
+            val expression = input.toMathExpression()
+            if (input.contains("=")) {
+                println("Variable successfully defined, variable currently evaluates to " + expression.evaluate().value)
+            } else {
+                println("The result is " + expression.evaluate().value)
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
